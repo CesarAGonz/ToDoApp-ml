a791390b38fa8e3dee96df41c769ee941c89d4c3
CesarAGonz <76890038+CesarAGonz@users.noreply.github.com>
2024-10-09 12:50:52 -0600

    add dist

---
+++ dist/controllers/task.controller.js
@@ -0,0 +1,57 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteTask = exports.updateTask = exports.createTask = exports.getTasks = void 0;
+const database_1 = require("../config/database");
+const getTasks = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const [tasks] = yield database_1.db.query('SELECT * FROM tasks WHERE userId = ?', [req.userId]);
+        res.json(tasks);
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error retrieving tasks', error });
+    }
+});
+exports.getTasks = getTasks;
+const createTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const newTask = Object.assign(Object.assign({}, req.body), { userId: req.userId });
+        yield database_1.db.query('INSERT INTO tasks SET ?', [newTask]);
+        res.status(201).json({ message: 'Task created' });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error creating task', error });
+    }
+});
+exports.createTask = createTask;
+const updateTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const taskId = req.params.id;
+        const updatedTask = req.body;
+        yield database_1.db.query('UPDATE tasks SET ? WHERE id = ? AND userId = ?', [updatedTask, taskId, req.userId]);
+        res.json({ message: 'Task updated' });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error updating task', error });
+    }
+});
+exports.updateTask = updateTask;
+const deleteTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const taskId = req.params.id;
+        yield database_1.db.query('DELETE FROM tasks WHERE id = ? AND userId = ?', [taskId, req.userId]);
+        res.json({ message: 'Task deleted' });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error deleting task', error });
+    }
+});
+exports.deleteTask = deleteTask;
---
+++ dist/routes/task.routes.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const task_controller_1 = require("../controllers/task.controller");
+const auth_middleware_1 = require("../middlewares/auth.middleware");
+const router = (0, express_1.Router)();
+router.get('/', auth_middleware_1.authenticate, task_controller_1.getTasks);
+router.post('/', auth_middleware_1.authenticate, task_controller_1.createTask);
+router.put('/:id', auth_middleware_1.authenticate, task_controller_1.updateTask);
+router.delete('/:id', auth_middleware_1.authenticate, task_controller_1.deleteTask);
+exports.default = router;
